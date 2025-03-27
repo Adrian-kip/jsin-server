b4e130af6a5d17dd6e6439d72ad9ddde612e90ec
Adrian Kiptoo <cherukip.adrian@gmail.com>
2025-03-27 10:39:02 +0300

    Added bootcamps data and updated server to serve JSON

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello from the server!' });
+// Route to serve the bootcamps JSON data
+app.get('/bootcamps', (req, res) => {
+  const filePath = path.join(__dirname, 'bootcamps.json');
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Error reading data file');
+      return;
+    }
+    res.json(JSON.parse(data));
+  });
 });
 
 app.listen(PORT, () => {
